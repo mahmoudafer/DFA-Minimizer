1123aca7bbea74bbd8c6241ef8933aba62bbf67d
mahmoudafer <mahmoudafer1@gmail.com>
2019-11-25 22:28:19 +0200

    added insert function to DAFSA class

--- dafsa.js
+++ dafsa.js
@@ -1,1 +1,22 @@
-const node = ()
+const node = (height, id) => {
+    return {
+        height,
+		id,
+		alphabets: {}
+    }
+}
+
+class dafsa {
+    constructor () {
+        this.id = 0
+        this.head = node(-1, 0)
+	}
+	
+	insert (letter) {
+		let current = this.head
+		while (current.alphabets[letter])
+			current = current.alphabets[letter]
+		current.alphabets[letter] = node(current.height, this.id)
+		this.id++
+	}
+}
