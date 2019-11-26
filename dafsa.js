589230ccb0007c9fc376d928f89a8c79ab9b2acf
mahmoudafer <mahmoudafer1@gmail.com>
2019-11-26 02:10:41 +0200

    dafsa added insert string

--- dafsa.js
+++ dafsa.js
@@ -1,22 +1,45 @@
-const node = (height, id) => {
+const readline = require('readline')
+
+const node = (height, id, final = false) => {
     return {
         height,
 		id,
+		final,
 		alphabets: {}
     }
 }
 
 class dafsa {
     constructor () {
-        this.id = 0
+        this.id = 1
         this.head = node(-1, 0)
 	}
+
+	insertString (str) {
+		let i, current = this.head
+		for (i = 0; i < str.length - 1; i++) {
+			current = this.insertLetter(str.charAt(i), current)
+		}
+		this.insertLetter(str.charAt(i), current, true)
+	}
 	
-	insert (letter) {
-		let current = this.head
-		while (current.alphabets[letter])
-			current = current.alphabets[letter]
-		current.alphabets[letter] = node(current.height, this.id)
-		this.id++
+	insertLetter (letter, current, final) {
+		if (!current.alphabets[letter]) {
+			current.alphabets[letter] = node(current.height - 1, this.id, final)
+			this.id++
+		}
+		return current.alphabets[letter]
 	}
 }
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+const dafsaInst = new dafsa()
+
+rl.question('Enter a string: ', (input) => {
+	console.log(`Received: ${input}`)
+	dafsaInst.insertString(input)
+})
